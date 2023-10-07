950c4f87c263db5d42565ef660b3c3c9756c54d4
Jonathan47G <guestin.jonathan@gmail.com>
2023-10-08 00:09:33 +0200

    Filtre, éviter les doublon, ajout affichage

--- src/components/ListeLogements.jsx
+++ src/components/ListeLogements.jsx
@@ -1,18 +1,27 @@
 import ListeLogement from "../datas/listeDeLogement.json";
-console.log(ListeLogement);
 
 function CreationListeDeLogement() {
-  return (
-    <div>
-      <ul className="lmj-plant-list">
-        {ListeLogement.map((logement) => (
-          <li key={logement.id} className="card-logement">
-            {logement.host.name}<img src={logement.cover} alt="couverture appartement"/>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  const ListeLogementFiltre = ListeLogement.reduce((accumulateur, logement) => { 
+    const ListeLogementTraitement = accumulateur.find((id) => id === logement.id);
+  
+    if (!ListeLogementTraitement) {
+      accumulateur.push(logement);
+    }
+  
+    return accumulateur;
+  }, []);
+  console.log(ListeLogementFiltre)
+    return (
+      <div>
+        <ul className="lmj-plant-list">
+          {ListeLogementFiltre.map((logement) => (
+            <li key={logement.id} className="card-logement">
+              {logement.host.name}<img src={logement.cover} alt="couverture appartement"/>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
 }
 
 export default CreationListeDeLogement;
