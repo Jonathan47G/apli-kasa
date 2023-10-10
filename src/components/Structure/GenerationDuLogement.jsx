52d71333ef2eab13387362317895de8c7a56a0fd
Jonathan47G <guestin.jonathan@gmail.com>
2023-10-10 15:28:44 +0200

    Mise en place des collapse detail logement

--- src/components/Structure/GenerationDuLogement.jsx
+++ src/components/Structure/GenerationDuLogement.jsx
@@ -2,21 +2,45 @@ import React from "react";
 import GenerationDuSlider from "./Slider";
 import RecuperationDeIdActuel from "../Fonctionnel/RecuperationIdDeLaPage";
 import GenerationDonneeJson from "../Fonctionnel/GenerationDonneeLogement";
+import Collapse from "../Fonctionnel/Collapse";
 
 function GenerationDeLaPageLogement() {
-  const logement = RecuperationDeIdActuel(); 
-  const rating = logement.rating;
-  const titre = logement.title;
-  const equipment = logement.equipments;
+  const logement = RecuperationDeIdActuel();
+  const noteDuLogement = logement.rating;
+  const titreDuLogement = logement.title;
+  const tagDuLogement = logement.tags;
+  const equipmentDuLogement = logement.equipments;
+  const nomHote = logement.host.name;
+  const imageHote = logement.host.picture;
+  const descriptionDuLogement = logement.description;
 
   return (
-    <>
-      <article>
-        <div><GenerationDonneeJson dataJson={rating} className="rating" /></div>
-        <div><GenerationDonneeJson dataJson={titre} className="titre"/></div>
-        <div><GenerationDonneeJson dataJson={equipment} className="liste-equipment"/></div>
-      </article>
-    </>
+    <article>
+      <GenerationDonneeJson dataJson={titreDuLogement} className="titre" />
+      <div>
+        <GenerationDonneeJson dataJson={nomHote} className="nom-host" />
+        <img src={imageHote} alt={`Appartement de ${nomHote}`} />
+      </div>
+      <GenerationDonneeJson dataJson={tagDuLogement} className="liste-tag" />
+      <div>
+        <GenerationDonneeJson dataJson={noteDuLogement} className="rating" />
+      </div>
+      <Collapse
+          titre="Description"
+          contenu={
+            <GenerationDonneeJson dataJson={descriptionDuLogement} className="description" />
+          }
+          maxHeight={"70px"}
+        />
+      <Collapse
+          titre="Équipements"
+          contenu={
+            <GenerationDonneeJson dataJson={equipmentDuLogement} className="liste-equipment" />
+          }
+          maxHeight={"200px"}
+        />
+      
+    </article>
   );
 }
 
